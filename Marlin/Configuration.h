f3a6419109c25677bcb4eb20550050778d7ceee8
Jerry Sweeney <JDSween855@pm.me>
2024-02-23 13:24:54 -0500

    Changed back. Build really didn't like it. Why?

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -2214,7 +2214,7 @@
 // :[-1,1]
 #define X_HOME_DIR -1
 #define Y_HOME_DIR -1
-#define Z_HOME_DIR 1 //JDS
+#define Z_HOME_DIR -1 //JDS
 // #define I_HOME_DIR -1
 // #define J_HOME_DIR -1
 // #define K_HOME_DIR -1
